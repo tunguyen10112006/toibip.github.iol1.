ee853b024fcb00f84d130f132229913b683f1b4c
tunguyen10112006 <nguyenviettuantu82@gmail.com>
2024-12-02 15:03:49 +0700

    Add files via upload

---
+++ external.js
@@ -0,0 +1,10 @@
+// hiển thị một dòng văn bản
+document.write("hello ")
+//hiển thị hộp thoại thông báo 
+alert("Chào mừng  các bạn đến với javaScript!\n Bấm ok để nghe pickleball ");
+// nhạp giữ liệu
+var name = prompt("Nhập tên của bạn")
+document.write("chào bạn "+ name +" .chúc bạn chim như lo xo!");
+var question = confirm("Bạn thực sự muốn truy cập vào trang Jav.com này ?")
+if(question !="0")
+    top.location = "https://zingmp3.vn/album/Pickleball-Single-DO-PHU-QUI/6C698UDD.html"
